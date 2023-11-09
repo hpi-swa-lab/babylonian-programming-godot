022b056cc469c0ddeea04d943a8dd329b207eaaf
Marius Dörbandt <github.marius@doerbandt.de>
2023-11-09 18:21:23 +0100

    Adjust styling of TextAnnotation

--- addons/watch/watch.gd
+++ addons/watch/watch.gd
@@ -150,6 +150,10 @@ class TextAnnotation extends Annotation:
 		node.fit_content = true
 		node.text_direction = Control.TEXT_DIRECTION_LTR
 		node.autowrap_mode = TextServer.AUTOWRAP_OFF
+		node.add_theme_font_size_override("normal_font_size", 30)
+		var style_box = StyleBoxFlat.new()
+		style_box.bg_color = Color.TRANSPARENT
+		node.add_theme_stylebox_override("normal", style_box)
 	
 	func update_value(value: Variant):
 		node.text = str(value)
