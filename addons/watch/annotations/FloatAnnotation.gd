ab55cd2ec31c2184ee8c7b130b87651db09ed4c3
Marius Dörbandt <github.marius@doerbandt.de>
2023-11-19 16:53:09 +0100

    Increase TabDisplay height

--- addons/watch/annotations/FloatAnnotation.gd
+++ addons/watch/annotations/FloatAnnotation.gd
@@ -13,7 +13,7 @@ static func can_display(value: Variant) -> bool:
 func _init(_line: int, _text_edit: TextEdit):
 	super(_line, _text_edit)
 	var tab_display = TabDisplay.new()
-	tab_display.set_size(Vector2(500, 3 * get_line_height()))
+	tab_display.set_size(Vector2(500, 5 * get_line_height()))
 	tab_display.add_tab("Graph", FloatOverTimeDisplay.new())
 	tab_display.add_tab("Current", TextDisplay.new())
 	display = tab_display
