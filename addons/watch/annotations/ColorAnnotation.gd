a0a5fbbcdda6e66c7509c21918b08bd6fa7e2bb5
Marius Dörbandt <github.marius@doerbandt.de>
2023-11-28 15:12:17 +0100

    Use CycleDisplay in ColorAnnotation

--- addons/watch/annotations/ColorAnnotation.gd
+++ addons/watch/annotations/ColorAnnotation.gd
@@ -14,5 +14,8 @@ func _init(_line: int, _text_edit: TextEdit):
 	super(_line, _text_edit)
 	var popup = PopupDisplay.new()
 	display = popup
-	var color_editor = popup.set_inner(ColorEditorDisplay.new())
+	var cycle = popup.set_inner(CycleDisplay.new())
+	var color = cycle.add_display(ColorDisplay.new())
+	color.set_size(get_line_height_square())
+	var color_editor = cycle.add_display(ColorEditorDisplay.new())
 	color_editor.set_size(get_line_height_square())
