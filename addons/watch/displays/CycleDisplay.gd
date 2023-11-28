fe9855020ed610058bdd59220056f9b0c11da50f
Marius Dörbandt <github.marius@doerbandt.de>
2023-11-28 15:08:38 +0100

    Add CycleDisplay

---
+++ addons/watch/displays/CycleDisplay.gd
@@ -0,0 +1,29 @@
+extends Display
+class_name CycleDisplay
+
+var index = 0
+var displays: Array[Display]
+
+func _init():
+	var button = Button.new()
+	button.text = "↔"
+	button.pressed.connect(self.toggle)
+	add_child(button)
+
+func toggle():
+	if len(displays) == 0:
+		return
+	remove_child(displays[index])
+	index += 1
+	index %= len(displays)
+	add_child(displays[index])
+
+func add_display(display: Display):
+	if len(displays) == 0:
+		add_child(display)
+	displays.append(display)
+	display.annotation = annotation
+
+func update_value(new_value: Variant):
+	for display in displays:
+		display.update_value(new_value)
