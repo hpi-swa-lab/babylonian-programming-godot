4362c52b96365d6ed24af00c4f6ba669bbcd4e8e
Marius Dörbandt <github.marius@doerbandt.de>
2024-01-02 15:08:11 +0100

    Scale font size with TextDisplay size

--- addons/watch/displays/TextDisplay.gd
+++ addons/watch/displays/TextDisplay.gd
@@ -7,11 +7,15 @@ func _ready():
 	label = Label.new()
 	label.text_direction = Control.TEXT_DIRECTION_LTR
 	label.autowrap_mode = TextServer.AUTOWRAP_OFF
-	label.add_theme_font_size_override("font_size", size.y)
 	var style_box = StyleBoxFlat.new()
 	style_box.bg_color = Color.TRANSPARENT
 	label.add_theme_stylebox_override("normal", style_box)
 	add_child(label)
+	resized.connect(self.update_font_size)
+	update_font_size()
+
+func update_font_size():
+	label.add_theme_font_size_override("font_size", size.y)
 
 func update_value(new_value: Variant):
 	label.text = str(new_value)
