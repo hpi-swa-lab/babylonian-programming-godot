2a205d14585b8e4a3e81db598f7ab81c5080be40
Marius Dörbandt <github.marius@doerbandt.de>
2024-01-09 10:57:06 +0100

    Remove onready from start_position

--- mario.gd
+++ mario.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = -600.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var start_position = global_position
+var start_position = global_position
 
 func _ready():
 	$Sprite.play()
