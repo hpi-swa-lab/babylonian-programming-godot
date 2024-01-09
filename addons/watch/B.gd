944232f1d227e55e32b12e8dc7a9988f31509991
Marius Dörbandt <github.marius@doerbandt.de>
2024-01-09 11:55:16 +0100

    Add show_in_game parameter to B.watch

--- addons/watch/B.gd
+++ addons/watch/B.gd
@@ -8,15 +8,16 @@ func _ready():
 func _process(delta):
 	watch_manager.update()
 
-func watch(value: Variant):
+func watch(value: Variant, show_in_game = false):
 	var stack = get_stack()
 	if len(stack) < 2:
 		print("Watching is unsupported without access to a stack")
-		return
+		return value
 	var location = stack[1] # get the caller
 	var source = location["source"]
 	var line = location["line"]
 	var data = [source, line, value]
 	EngineDebugger.send_message("watch:watch", data)
-	watch_manager.on_watch(source, line, value)
+	if show_in_game:
+		watch_manager.on_watch(source, line, value)
 	return value
