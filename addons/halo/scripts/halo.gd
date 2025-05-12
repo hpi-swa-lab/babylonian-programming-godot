ea856bd37b08ed1fba5dc15588ff4cc2f3b1dc9c
Christian Raue <captain_2@gmx.de>
2025-05-12 19:27:06 +0200

    area bounding box, additional buttons, undo, copy to clipboard

--- addons/halo/scripts/halo.gd
+++ addons/halo/scripts/halo.gd
@@ -9,12 +9,15 @@ class_name Halo extends Node2D
 @onready var inspect_button: TextureButton = $InspectButton
 @onready var tree_visibility_button: TextureButton = $TreeVisibilityButton
 @onready var window: Window = $HaloInspectorWindow
+@onready var selection_rect: ColorRect = $SelectionRect
 
 @onready var name_tag: Label = $NameTag
 
-@export var target: CanvasItem = null
+@export var target: Node = null
 
 var buttons: Array = []
+var additional_buttons: Array = []
+
 var tree_lines: Array = []
 var show_tree_lines: bool = false
 
@@ -28,20 +31,31 @@ var dragging: bool = false
 var dragging_v: bool = false
 var dragging_h: bool = false
 var rotating: bool = false
-var desired_rotation: float = INF
 
-func _ready() -> void:
-	self.window.visible = false
+var target_is_root: bool = false	
 
-func set_target(target: CanvasItem) -> void:
+func set_target(target: CanvasItem, target_is_root: bool, additional_buttons: Array) -> void:
 	self.target = target
-	self.name_tag.text = self.target.get_class() + " (" + str(self.get_depth()) + ")"
+	self.target_is_root = target_is_root
+	self.additional_buttons = additional_buttons
+	for button in additional_buttons:
+		if not self.is_ancestor_of(button):
+			self.add_child(button)
+	
+	self.name_tag.text = self.target.name + " (" + str(self.get_depth()) + ")"
+	self.window.visible = false 
 	self.window.title = self.target.get_class()
 	self.target.item_rect_changed.connect(reposition)
 	var inspector = self.window.get_node("Inspector")
 	inspector.set_object(self.target)
-	inspector.update_inspector()
-	inspector._update_property_list()
+	
+	self.move_button.visible = not self.target_is_root
+	self.move_v_button.visible = not self.target_is_root
+	self.move_h_button.visible = not self.target_is_root
+	self.rotate_button.visible = not self.target_is_root
+	self.reset_rotation_button.visible = not self.target_is_root
+	
+	self.place_buttons()
 	self.reposition()
 	self.place_tree_lines()
 	
@@ -58,39 +72,62 @@ func set_tree_line_visibility(visibility: bool) -> void:
 	self.show_tree_lines = visibility
 	for tree_line in self.tree_lines:
 		tree_line.visible = visibility
-		
-func get_button_array() -> Array:
-	return [
-		delete_button,
-		move_button,
-		move_v_button,
-		move_h_button,
-		rotate_button,
-		reset_rotation_button,
-		inspect_button,
-		tree_visibility_button
-	]
-		
-func reposition() -> void:
-	buttons = self.get_button_array()
+	
+func place_buttons() -> void:
+	self.buttons = []
+	if not self.target_is_root:
+		self.buttons.append_array([
+			self.move_button,
+			self.move_v_button,
+			self.move_h_button,
+			self.rotate_button,
+			self.reset_rotation_button
+		])
+	self.buttons.append_array([
+		self.inspect_button,
+		self.tree_visibility_button,
+		self.delete_button
+	])
+	self.buttons.append_array(self.additional_buttons)
 	
 	for i in buttons.size():
 		var angle: float = i * 2 * PI / buttons.size()
-		buttons[i].global_position = Vector2(
+		buttons[i].position = Vector2(
 			cos(angle) * self.RADIUS,
 			sin(angle) * self.RADIUS
-		) + self.target.global_position - (buttons[i].size * buttons[i].scale) / 2
+		) - (buttons[i].size * buttons[i].scale) / 2
 	var string_size: Vector2 = self.name_tag.theme.default_font.get_string_size(
 		self.name_tag.text, HORIZONTAL_ALIGNMENT_LEFT, -1, self.name_tag.theme.default_font_size
 	)
-	self.name_tag.global_position = Vector2(
+	self.name_tag.position = Vector2(
 		0, 
-		-sin(1.5 * PI) * 1.5 * RADIUS + self.name_tag.size.y
-	) + self.target.global_position - Vector2(string_size.x / 2, string_size.y * 2)
+		-sin(1.5 * PI) * RADIUS
+	) + Vector2(-string_size.x / 2, string_size.y)
+		
+func reposition() -> void:
+	if not self.target_is_root:
+		self.global_position = self.target.global_position
+	else:
+		self.global_position = Vector2.ZERO
+		
+	if self.target is Control:
+		self.selection_rect.size = self.target.get_global_rect().size
+		self.selection_rect.global_position = self.target.global_position - self.selection_rect.size / 2
+		self.selection_rect.visible = true
+	elif self.target is Sprite2D:
+		self.selection_rect.size = self.target.texture.get_size()
+		self.selection_rect.global_position = self.target.global_position - self.selection_rect.size / 2
+		self.selection_rect.visible = true
+	elif self.target is CollisionShape2D:
+		self.selection_rect.size = self.target.shape.get_rect().size
+		self.selection_rect.global_position = self.target.global_position - self.selection_rect.size / 2
+		self.selection_rect.visible = true
+	else:
+		self.selection_rect.visible = false
 	
 func place_tree_line(node: CanvasItem, is_parent: bool = false):
 	var tree_line: Line2D = preload("res://addons/halo/scenes/tree_line.tscn").instantiate()
-	tree_line.points[0] = self.position
+	tree_line.points[0] = to_local(self.target.global_position)
 	tree_line.points[1] = to_local(node.global_position)
 	var line_color = self.TREE_LINE_CHILD_COLOR
 	if is_parent:
@@ -137,9 +174,9 @@ func perform_dragging() -> void:
 	if self.dragging or self.dragging_h or self.dragging_v:
 		var new_position: Vector2 = (
 			get_global_mouse_position() 
-			- button.position - (
-				button.size * button.scale
-			) / 2
+			#- button.position - (
+				#button.size * button.scale
+			#) / 2
 		)
 		if self.dragging:
 			self.target.global_position = new_position
@@ -148,27 +185,21 @@ func perform_dragging() -> void:
 		elif self.dragging_h:
 			self.target.global_position.x = new_position.x
 			
+		self.reposition()
+			
 	if self.rotating:
 		var angle: float = atan2(
 			-self.target.global_position.y + get_global_mouse_position().y,
 			-self.target.global_position.x + get_global_mouse_position().x,
 		)
-		var buttons: Array = self.get_button_array()
-		var button_index: int = buttons.find(self.rotate_button)
+		var button_index: int = self.buttons.find(self.rotate_button)
 		var angle_offset: float = button_index * 2 * PI / buttons.size()
-		self.desired_rotation = angle - angle_offset
-		
-func handle_rotation() -> void:
-	if self.desired_rotation != INF:
-		self.target.rotation = self.desired_rotation
-		self.rotation = -self.desired_rotation
-		self.desired_rotation = INF
+		self.target.rotation = angle - angle_offset
 		
 func _process(delta: float) -> void:
 	self.perform_dragging()
 	self.reposition()
 	self.place_tree_lines()
-	self.handle_rotation()
 
 func _on_delete_button_pressed() -> void:
 	target.queue_free()
@@ -205,4 +236,4 @@ func _on_rotate_button_button_up() -> void:
 	self.rotating = false
 
 func _on_reset_rotation_button_pressed() -> void:
-	self.desired_rotation = 0
+	self.target.rotation = 0
--- addons/halo/scripts/halo_dispatcher.gd
+++ addons/halo/scripts/halo_dispatcher.gd
@@ -4,14 +4,25 @@ var halo_target: CanvasItem = null
 var halo: Node2D = null
 
 var show_tree_lines: bool = false
+var buttons: Dictionary = {}
+var next_button_id: int = 0
+
+var undo_stack: Array = []
+var redo_stack: Array = []
 
 const MOUSE_BUTTON: int = MOUSE_BUTTON_MIDDLE
 const UP_KEY: int = KEY_SHIFT
 const DOWN_KEY: int = KEY_CTRL
 const MAX_DISTANCE: int = 32
+const MODIFIER_KEY: int = KEY_CTRL
+const UNDO_KEY: int = KEY_Z
+const REDO_KEY: int = KEY_Y
 
 func _ready():
 	set_process_input(true)
+	
+func get_scene_root() -> Node:
+	return get_tree().get_root().get_child(get_tree().get_root().get_child_count() - 1)
 
 func _input(event: InputEvent) -> void:
 	if (
@@ -19,11 +30,42 @@ func _input(event: InputEvent) -> void:
 		and event.button_index == self.MOUSE_BUTTON
 		and event.pressed
 	):
-		var scene_root: Node = get_tree().current_scene
+		var scene_root: Node = self.get_scene_root()
 		var up_key_pressed: bool = Input.is_key_pressed(self.UP_KEY)
 		var down_key_pressed: bool = Input.is_key_pressed(self.DOWN_KEY)
-		var target: Node = find_target(scene_root, event.position, up_key_pressed, down_key_pressed)
-		self.set_target(target)			
+		var target: CanvasItem = find_target(scene_root, event.position, up_key_pressed, down_key_pressed)
+		self.set_target(target, scene_root)
+	elif (
+		event is InputEventKey
+		and event.pressed 
+		and Input.is_key_pressed(self.MODIFIER_KEY)
+	):
+		if event.keycode == self.UNDO_KEY:
+			self.undo()
+		elif event.keycode == self.REDO_KEY:
+			self.redo()
+			
+func undo() -> void:
+	var scene_root: Node = self.get_scene_root()
+	if self.undo_stack.is_empty():
+		self.set_target(null, scene_root, true)
+	var new_target: CanvasItem = self.undo_stack.pop_back()
+	if scene_root.is_ancestor_of(new_target) and is_instance_valid(new_target):
+		self.redo_stack.push_back(self.halo_target)
+		self.set_target(new_target, scene_root, true)
+	else:
+		self.undo_stack.clear()
+	
+func redo() -> void:
+	if self.redo_stack.is_empty():
+		return
+	var scene_root: Node = self.get_scene_root()
+	var new_target: CanvasItem = self.redo_stack.pop_back()
+	if scene_root.is_ancestor_of(new_target) and is_instance_valid(new_target):
+		self.undo_stack.push_back(self.halo_target)
+		self.set_target(new_target, scene_root, true)
+	else:
+		self.redo_stack.clear()
 			
 func get_world_position(screen_position: Vector2) -> Vector2:
 	var cam = get_viewport().get_camera_2d()
@@ -108,19 +150,44 @@ func find_target(root: Node, screen_pos: Vector2, go_up: bool, go_down: bool, ex
 	else:
 		return area_target
 
-func set_target(target: CanvasItem) -> void:
-	if self.halo:
-		self.halo.get_node("TreeVisibilityButton").button_down.disconnect(_on_tree_visibility_button_down)
-		self.halo.queue_free()
+func set_target(target: CanvasItem, scene_root: Node, is_undo_redo: bool = false) -> void:
+	if not self.halo:
+		self.halo = preload("res://addons/halo/scenes/halo.tscn").instantiate()
+		scene_root.add_child(self.halo)
+		self.halo.get_node("TreeVisibilityButton").button_down.connect(_on_tree_visibility_button_down)
+		
+	if self.halo_target and not is_undo_redo:
+		self.undo_stack.push_back(self.halo_target)
 		
 	self.halo_target = target
-	
-	if self.halo_target:
-		self.halo = preload("res://addons/halo/scenes/halo.tscn").instantiate()
-		self.halo_target.add_child(self.halo)
-		self.halo.set_target(self.halo_target)
+	if target:
+		self.halo.set_target(self.halo_target, self.halo_target == scene_root, self.buttons.values())
 		self.halo.set_tree_line_visibility(self.show_tree_lines)
-		self.halo.get_node("TreeVisibilityButton").button_down.connect(_on_tree_visibility_button_down)
+		self.halo.visible = true
+	else:
+		self.halo.visible = false
+		
+	if not is_undo_redo:
+		self.redo_stack.clear()
+		
+func add_button(texture: Texture2D, callback: Callable, NodeTypes: Array = [], color_modulation: Color = Color.WHITE) -> int:
+	var button_signal_handler = func() -> void:
+		callback.call(self.halo_target)
+	
+	var button: TextureButton = TextureButton.new()
+	button.texture_normal = texture
+	button.modulate = color_modulation
+	button.size = Vector2(50, 50)
+	button.scale = Vector2(0.2, 0.2)
+	button.z_index = 128
+	button.pressed.connect(button_signal_handler)
+	
+	self.buttons[self.next_button_id] = button
+	self.next_button_id += 1
+	return self.next_button_id - 1
+	
+func remove_button(button_id: int) -> void:
+	self.buttons.erase(button_id)
 		
 func _on_tree_visibility_button_down() -> void:
 	self.show_tree_lines = not self.show_tree_lines
