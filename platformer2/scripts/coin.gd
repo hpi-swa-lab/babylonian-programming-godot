393805ab60b43bbf0cd9d8ad3167239b009402c2
Christian Raue <captain_2@gmx.de>
2025-06-19 17:10:48 +0200

    find game_manager

--- platformer2/scripts/coin.gd
+++ platformer2/scripts/coin.gd
@@ -1,7 +1,23 @@
 extends Area2D
 
-@onready var game_manager: Node = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var game_manager: Node
+
+#func _ready():
+	#game_manager = $"../../GameManager"
+
+func find_game_manager(node):
+	if node.name == "GameManager":
+		return node
+	for child in node.get_children():
+		var result = find_game_manager(child)
+		if result:
+			return result
+	return null
+
+func _ready():
+	game_manager = find_game_manager(get_tree().get_root())
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
