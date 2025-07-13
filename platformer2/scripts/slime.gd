17d8679dea4e6f2215cc9d6f8cd4d6b8fac5c32d
Christian Raue <captain_2@gmx.de>
2025-07-13 14:08:52 +0200

    renaming

--- platformer2/scripts/slime.gd
+++ platformer2/scripts/slime.gd
@@ -4,7 +4,7 @@ class_name Slime extends Node2D
 const SPEED: int = 60
 
 var _direction: int = 1
-var _move: bool = true
+var _is_moving: bool = true
 static var _initialized: bool = false
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
@@ -13,16 +13,16 @@ static var _initialized: bool = false
 
 func _add_button() -> void:
 	var button_handler = func(target: CanvasItem) -> void:
-		target._move = not target._move
+		target._is_moving = not target._is_moving
 		
 	var atlas: Texture2D = load("res://addons/babylonian/halo/assets/ui_0.png")
-	var region := Rect2( Vector2(100, 450), Vector2(50, 50) )
-	var region_texture := AtlasTexture.new()
-	region_texture.atlas = atlas
-	region_texture.region = region
+	var region: Rect2 = Rect2( Vector2(100, 450), Vector2(50, 50) )
+	var texture: Texture2D = AtlasTexture.new()
+	texture.atlas = atlas
+	texture.region = region
 		
 	HaloDispatcher.button_manager.add_button(
-		region_texture, 
+		texture, 
 		button_handler,
 		["Slime"],
 		Color.ORANGE
@@ -36,7 +36,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if self._move:
+	if self._is_moving:
 		if ray_cast_right.is_colliding():
 			_direction = -1
 			animated_sprite.flip_h = true
